03133f25c6e04e18802d4d712e2da4dca1760832
Breyar <breyaralvarez@gmail.com>
2024-03-06 16:31:29 -0600

    Borrando contenido en la nueva rama

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("hola breyar")
+//hasme un algoritmo de una calculadora.
+Fuimos testigos de una época en la que tener acceso a información valiosa era un privilegio. platzi nace con el propósito de transformar la vida de las personas a través de educación profesional efectiva.
+
+Nos emociona descubrir que nuestros estudiantes dan pasos gigantes hacia un futuro lleno de nuevas oportunidades. Personas con ideas increíbles que logran hacerlas realidad y al ser parte de nuestra comunidad encuentran más posibilidades de crecimiento o desarrollo de otros talentos.
+
+Tú también puedes ser una de ellas. ¿Te interesa saber más?
+
+Qué es y qué hace platzi
+platzi es una de las plataformas de educación profesional en línea más grandes de América Latina, fundada en 2011 por Freddy Vega y Christian Van der Henst. Hoy tenemos 3 millones de estudiantes activos en más de 140 países, miles de cursos y áreas de estudio enfocadas en el desarrollo de habilidades y conocimientos que requieren las grandes empresas del mundo.
+
